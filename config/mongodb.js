b21225ad83ac165e3fbb086b25a3c651bba0aad2
KRISHNA KUMAR <KRISHNA.EDU.HUB@GMAIL.COM>
2022-08-17 20:43:46 +0530

    api is working

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,16 +1,8 @@
-const mongoose = require("mongoose");
+
 // HERE ENV IS IN USED
-require("dotenv").config();
+
 
 const connectionString = process.env.MONGO_DB_URL;
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-mongoose.connection.on("connected", () => {
-  console.log("connected to database");
-});
 
-module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
-const FriendsApi = require("./api/FriendsApi");
+const cors = require("cors");
+const FriendModels = require("./models/FriendModels");
+const mongoose = require("mongoose");
+require("dotenv").config();
 // import database and just call
 require('./config/mongodb');
 var favicon = require('serve-favicon');
@@ -9,12 +11,21 @@ var path = require('path');
 require("dotenv").config();
 
 // call from api\FriendsApi.js
-app.use(FriendsApi);
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 // accept all the json data
 app.use(express.json());
 // use cors 
 
+app.use(cors());
+
+mongoose.connect("mongodb+srv://krishna001:Password@merncluster.aepwq.mongodb.net/friends_db?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to database");
+});
 
 const portNumber = process.env.PORT_URL;
 
@@ -23,6 +34,97 @@ app.listen(portNumber, () => {
 });
 
 
+// as per your requirement import the model
+app.get("/", (req, res) => {
+  // show mesasge in bold font and in green color
+  // res.send({
+  //   message: "Welcome to the API",
+  //   status: "success",
+  //   publicAccess: "yes",
+  // });
+
+  //  add center add style to the message
+  res.send(`<center>
+  <h1 style="color:green">Welcome to the API</h1>
+  <h2> Server is running Live </h2>
+  
+  </center>`);
+});
+
+// post
+
+app.post("/addFriends", async (req, res) => {
+  // make dynamic url
+  const userExists = await FriendModels.findOne({ email: req.body.email });
+  if (userExists) {
+    res.send({ "actualData": userExists, "message": `An Account with Email ${userExists.email} already Exists` });
+
+  } else {
+    const friend = new FriendModels({
+      // name: req.body.name,
+      // age: req.body.age,
+      // desc: req.body.desc,
+      ...req.body,
+    });
+    try {
+      const savedFriend = await friend.save();
+      res.send({ "actualData": savedFriend, "status": "success", "message": "Friend added successfully" });
+    } catch (error) {
+      res.send({ "actualData": error, "status": "failed", "message": "Something went wrong" });
+    }
+  }
+}),
+
+  // read
+  app.get("/read", async (req, res) => {
+    // two parameter is required to  get data, either it will use or not(req,res)
+    const friends = await FriendModels.find();
+    //   if you will concatenate with string , it will not consider a valid json
+    //   res.send("data fetched" + friends);
+    res.send(friends);
+    //   2nd method
+    //   with error handle
+    //  try {
+    //     const friends = await FriendModels.find();
+    //     res.send(friends);
+    //   } catch (err) {
+    //     res.send(err);
+    //   }
+  });
+
+// get data by id
+app.get("/getFriendById/:id", async (req, res) => {
+  const friend = await FriendModels.findById(req.params.id);
+  res.send(friend);
+});
+
+// delete data by id
+app.delete("/deleteFriendById/:id", async (req, res) => {
+  const deletedFriend = await FriendModels.findByIdAndDelete(req.params.id);
+  res.send({ "actualData": deletedFriend, "status": "success", "message": "Friend Deleted successfully" });
+}
+);
+// update
+app.put("/updateFriendById/:id", async (req, res) => {
+  //  handle error
+  try {
+    const updatedFriend = await FriendModels.findByIdAndUpdate(req.params.id, req.body);
+    //  get modified data from database then sent it to frontend
+    res.send({ "actualData": updatedFriend, "status": "success", "message": "Friend Updated successfully" });
+  }
+  catch (error) {
+    res.send({ "actualData": error, "status": "failed", "message": "Something went wrong" });
+  }
+}
+);
+
+
+// delete all the data from database
+app.delete("/deleteAll", async (req, res) => {
+  const deletedAll = await FriendModels.deleteMany();
+  res.send({ "actualData": deletedAll, "status": "success", "message": "All Friends Deleted successfully" });
+}
+);
 
 
 
